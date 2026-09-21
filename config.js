3cb9a065c10e7781ed022d9dd5b87d2281f731c4
Internship2006-svg <manav.manocha@lendahandindia.org>
2026-09-21 17:29:04 +0530

    config.js

--- config.js
+++ config.js
@@ -4,5 +4,5 @@
 */
 
 window.LAHiConfig = {
-  GOOGLE_SHEETS_URL: ""
+  GOOGLE_SHEETS_URL: "https://script.google.com/macros/s/AKfycbwWYafYY2rg6LqA6AQNUyPVp8_LFc8mvaieYO3Aw3ddteMlV-D4q4TyFIc82-SP884/exec"
 };
