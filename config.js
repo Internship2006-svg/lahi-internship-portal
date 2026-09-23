57b36d63107ffd350499d604500194afce8f102f
Internship2006-svg <manav.manocha@lendahandindia.org>
2026-09-23 12:22:36 +0530

    config.js

--- config.js
+++ config.js
@@ -1,8 +1,12 @@
-/* LAHI Portal configuration
-   Paste your deployed Google Apps Script Web App /exec URL below.
-   Leave it blank while testing the portal locally.
-*/
+/* LAHI Portal configuration */
 
 window.LAHiConfig = {
-  GOOGLE_SHEETS_URL: "https://script.google.com/macros/s/AKfycbwWYafYY2rg6LqA6AQNUyPVp8_LFc8mvaieYO3Aw3ddteMlV-D4q4TyFIc82-SP884/exec"
+  /*
+    After deploying Google Apps Script as a Web App,
+    paste the URL ending in /exec here.
+
+    Example:
+    GOOGLE_SHEETS_URL: "https://script.google.com/macros/s/XXXXXXXX/exec"
+  */
+  GOOGLE_SHEETS_URL: "https://script.google.com/macros/s/AKfycbwfMubDIOaVNO3zUFR9lJRxlF-vx8oUKRwBQkUEACm8p_ZjhkhjaBorx18DIk29HDWHuA/exec"
 };
